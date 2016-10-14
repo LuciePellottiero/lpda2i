2ae5ee2499a8e0f833b57cc13efa7401552acdd2
cedric <cedric@DESKTOP-Q9OJF57>
2016-10-14 11:54:50 +0200

    pull  test

--- Ipda2i/src/Main.java
+++ Ipda2i/src/Main.java
@@ -8,5 +8,6 @@ public class Main {
 	public static void main(String args[]){
 		System.out.println("Salut !");
 		System.out.println("Oui allo bonsoir ?");
+		System.out.println("pull pour test");
 	}
 }
